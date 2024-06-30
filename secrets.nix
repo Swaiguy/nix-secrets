6b2d900cd6efdcc92b41912707158b3b71523d64
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-01 01:33:27 +0200

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,10 @@
  
 ##Krys Imput.. idols_ai? whatever, i need to personalise all this later
   let
-    idols_ai = "ssh-ed25519 123 root@kry";
+    idol_ai = "ssh-ed25519 123 root@kry";
     recovery_key = "ssh-ed25519 123 ryan@agenix-recovery";
     systems = [
-    idols_ai
+    idol_ai
 
     recovery_key
     ];
