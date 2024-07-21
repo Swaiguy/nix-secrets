e7ec452d962bf1af2f061cbe7bf5cd12f3524286
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-21 19:40:14 +0200

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,12 +1,18 @@
  
 ##Krys Imput.. idols_ai? whatever, i need to personalise all this later
+   ##and so 1 week later i am.
   let
-    idols_ai = "ssh-ed25519 123 root@kry";
-    recovery_key = "ssh-ed25519 123 kry@agenix-recovery";
+    
+    pc-ai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHT9eg1x9gNcVvFSaxyOWNatvUfw+HWAhsAKpj455gw root@pc";
+
+    recovery_key = "SHA256:JQlyeyGfOeeDdMVc3sQ2doFGdLUrunl8VuaZ1VGJCmA kry@agenix-recovery";
+    
     systems = [
-    idols_ai
+    
+    pc-ai
 
     recovery_key
+    
     ];
     {
       "./xxx.age".publicKeys = users ++ systems;
